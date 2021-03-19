39390942f0ee5306745ce2d1f07de17530140533
Alain Nkazamurego <alpacii@live.com>
2021-03-19 16:54:50 +0200

    Repository Updated

--- auth_user_post.php
+++ auth_user_post.php
@@ -1,9 +1,21 @@
-<?php include('db.php'); /*We call database options*/
+<?php include('db_conect.php'); /*We call database options*/
     if(isset($_GET['register']) OR (isset($_GET['login'])){
-
+       $job;
+       $id;
+       $names;
+       $access;
+       $level;
+       $temp;
+       
+        $rep -> job = $job;
+        $rep -> id = $id;
+        $rep -> names = $names;
+        $rep -> access = $access;
+        $rep -> level =  $level;
+        $rep -> temp = $temp;
     }else{
-        die(){
-            
-        }
+        $rep -> job ='no';
+        $rep -> msg ='Bad Request!';
     }
+die(json_encode($rep));
 ?>
