29b9f0741246602ef1cafe1e0d9f076b095a201a
Alain Nkazamurego <alpacii@live.com>
2021-03-19 19:47:20 +0200

    just removed the test in order to use postman

--- auth_user_post.php
+++ auth_user_post.php
@@ -21,22 +21,35 @@
                 $job = 'no';
                 $msg = 'The '.$column.' "'.$username.'" is already registered!';
             }else{
-                $job = 'ok';
-                $addUser = $db -> query('INSERT INTO `users`('.$column.', `password`, `temp`) VALUES ( \''.$username.'\', \''.$password.'\', \''.$temp.'\'')  or die(print_r($db->errorinfo()));
+               $addUser = $db -> query('INSERT INTO `users` ('.$column.', `password`, `temp`) VALUES ( \''.$username.'\', \''.$password.'\', \''.$temp.'\') ') or die(print_r($db->errorinfo()));
                 if($addUser){
-                    $readUser = $db -> query('SELECT * FROM users WHERE '.$column.' = \''.$username.'\' AND `password` = \''.$password.'\'') or die(print_r($db->errorinfo()));
-                    $User = $readUser -> fetch();
-                    $access = '';
-                    $level = '';
-                    $id = '';
+                    $readUserId = $db -> query('SELECT * FROM users WHERE '.$column.' = \''.$username.'\' AND password = \''.$password.'\'') or die(print_r($db->errorinfo()));
+                    $UserId = $readUserId -> fetch();
 
+                    $user_id = $UserId['id'];
+                    $level = '1';
+                    $access = 'user';
+                    $last_login_date = date('Y-m-d H:i:s');
+                    if(!isset($device)){
+                        $device = 'desk';
+                    }
+                    $last_login_device = $device;
+                    $token = $temp;
+                    $addAuth = $db -> query('INSERT INTO `users_auth`( `user_id`, `level`, `access`, `last_login_date`, `last_login_device`, `token`) VALUES ( \''.$user_id.'\', \''.$level.'\', \''.$access.'\', \''.$last_login_date.'\', \''.$last_login_device.'\', \''.$token.'\')') or die(print_r($db->errorinfo()));
+                  
+                    $readUserAuth = $db -> query('SELECT * FROM users LEFT JOIN users_auth ON users.id = users_auth.user_id WHERE users.'.$column.' = \''.$username.'\' AND users.password = \''.$password.'\'') or die(print_r($db->errorinfo()));
+                    $UserAuth = $readUserAuth -> fetch();
+                    $access = $UserAuth['access'];
+                    $level = $UserAuth['level'];
+                    $id =  $user_id;
+                    $job = 'ok';
+                    $msg = 'Successfull register';
+                }else{
+                    $rep -> id = $id;
+                    $rep -> access = $access;
+                    $rep -> level =  $level;
+                    $rep -> temp = $temp;
                 }
-                $rep -> access = $access;
-                $rep -> level =  $level;
-                $rep -> temp = $temp;
-                $rep -> job = $job;
-                $rep -> id = $id;
-                
             }
 			
 		}else if($_GET['auth'] == 'login'){
@@ -49,8 +62,10 @@
            $rep -> names = $names;
 		}
     }else{
-        $rep -> job ='no';
-        $rep -> msg ='Bad Request!';
+        $job ='no';
+        $msg ='Bad Request!';
     }
+$rep -> job = $job;
+$rep -> msg  = $msg;
 die(json_encode($rep));
 ?>
--- test.php
+++
@@ -1,6 +0,0 @@
-<form method="post" action="/auth/register">
-    <input type="text" name="username" placeholder="Ëmail or phone number" required/>
-    <input type="hidden" name="type" value="email" reqired/>
-    <input type="password" name="password" placeholder="Password" required />
-    <button>Send</button>
-</form>
